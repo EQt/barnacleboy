cfa20be5c54770a4f748cf4ef5dff65ddac93845
Till Hartmann <till.hartmann@udo.edu>
2018-10-05 13:22:50 +0200

    refactoring

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,6 +6,8 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_pickle;
+#[macro_use]
+extern crate structopt;
 
 use image::Pixel;
 use itertools::Itertools;
@@ -14,7 +16,17 @@ use merfishtools::io::merfishdata::binary::Record;
 use merfishtools::io::merfishdata::Reader;
 use palette::{Hsla, Srgba};
 use std::collections::HashMap;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+struct Options {
+    #[structopt(name = "FILE", parse(from_os_str))]
+    input: PathBuf,
+    #[structopt(short = "b", long = "boundaries", parse(from_os_str))]
+    boundaries: Option<PathBuf>,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -34,13 +46,16 @@ struct Cell {
     nucleus_convex_hull_ratio: f64,
 }
 
+fn draw_points<P: image::Pixel, I: image::GenericImage<Pixel=P>>(img: &mut I, color: &P, points: &Vec<(u32, u32)>) {
+    points.iter().for_each(|(x, y)| img.get_pixel_mut(*x, *y).blend(&color));
+}
+
 fn main() {
-    // TODO use clap.g
-    let args: Vec<String> = std::env::args().collect();
-    let path = &args[1];
+    let opt = Options::from_args();
+    let merfish_path = &opt.input;
 
     // TODO: don't iterate twice.
-    let mut reader = MReader::from_file(path).unwrap();
+    let mut reader = MReader::from_file(merfish_path).unwrap();
     let [[min_x, max_x], [min_y, max_y]] = reader.records().into_iter()
         .map(|r| r.unwrap())
         .fold([[std::f32::MAX, std::f32::MIN], [std::f32::MAX, std::f32::MIN]], |acc, r| {
@@ -53,8 +68,30 @@ fn main() {
     let mut img = image::ImageBuffer::from_pixel(abs_width, abs_height, image::Rgba([0u8, 0u8, 0u8, 255u8]));
     println!("{}x{}", abs_width, abs_height);
 
+    let mut cytoplasm_boundaries = None;
+    let mut nucleus_boundaries = None;
+    // Get cell boundaries if available
+    if let Some(cell_boundaries_path) = &opt.boundaries {
+        let f = std::fs::File::open(cell_boundaries_path).unwrap();
+        let cell_boundaries: HashMap<String, Cell> = serde_pickle::from_reader(f).unwrap();
+        // Skip virtual/non-existent cells (?) as well as non-cell boundaries
+        cytoplasm_boundaries = Some(cell_boundaries.values()
+            .filter(|&cell| cell.edge_marker != 1.)
+            .flat_map(|cell| &cell.cytoplasm_boundary)
+            .map(|&(x, y)| { ((x as f32 - min_x).floor() as u32, (y as f32 - min_y).floor() as u32) })
+            .collect());
+        nucleus_boundaries = Some(cell_boundaries.values()
+            .filter(|&cell| cell.edge_marker != 1.)
+            .flat_map(|cell| &cell.nucleus_boundary)
+            .map(|&(x, y)| { ((x as f32 - min_x).floor() as u32, (y as f32 - min_y).floor() as u32) })
+            .collect());
+    }
+
+    let transparent_white_rgba = image::Rgba([255, 255, 255, 128]);
+    let transparent_white_lumaa = image::LumaA([255, 128]);
+
     // create plot for every single barcode
-    let mut reader = MReader::from_file(path).unwrap();
+    let mut reader = MReader::from_file(merfish_path).unwrap();
     let map: HashMap<u16, Vec<Record>> = reader.records().into_iter()
         .map(|r| r.unwrap())
         .map(|r| (r.barcode_id, r))
@@ -68,15 +105,22 @@ fn main() {
 //            let magnitude: u8 = (record.total_magnitude * 255.).floor() as u8;
             let color = Hsla::new(((key as f32) / 140.) * 360., 1.0, 0.5, 0.75);
             let color = Srgba::from(color);
-            let data = [(color.color.red * 255.) as u8, (color.color.green * 255.) as u8, (color.color.blue * 255.) as u8, (color.alpha * 255.) as u8];
-            let rgba = image::Rgba(data);
 
             let mut pixel: &mut image::Rgba<u8> = img.get_pixel_mut(x as u32, y as u32);
+            let data = [(color.color.red * 255.) as u8, (color.color.green * 255.) as u8, (color.color.blue * 255.) as u8, (color.alpha * 255.) as u8];
+            let rgba = image::Rgba(data);
             pixel.blend(&rgba);
 
             let mut pixel: &mut image::LumaA<u8> = barcode_img.get_pixel_mut(x as u32, y as u32);
-            let grey = image::LumaA([255, (record.total_magnitude * 255.).floor() as u8]);
-            pixel.blend(&grey);
+            let data = [255, (record.total_magnitude * 255.).floor() as u8];
+            let lumaa = image::LumaA(data);
+            pixel.blend(&lumaa);
+        }
+        if let Some(ref bounds) = cytoplasm_boundaries {
+            draw_points(&mut barcode_img, &transparent_white_lumaa, &bounds);
+        }
+        if let Some(ref bounds) = nucleus_boundaries {
+            draw_points(&mut barcode_img, &transparent_white_lumaa, &bounds);
         }
         if abs_height > abs_width {
             // We actually rotate the images 90° CW, so that width > height for better display properties.
@@ -84,40 +128,18 @@ fn main() {
         }
         let image_path = std::path::Path::new("img/");
         if !image_path.exists() {
-            std::fs::create_dir(&image_path);
+            std::fs::create_dir(&image_path).expect(&format!("Failed creating directory {:?}.", &image_path));
         }
         image::ImageLumaA8(barcode_img).save(format!("img/barcode_{}.png", key)).unwrap();
     }
 
-    let f = std::fs::File::open("data/cellBoundaries.pickle").unwrap();
-    let cell_boundaries: HashMap<String, Cell> = serde_pickle::from_reader(f).unwrap();
-    for (_cell_id, cell) in cell_boundaries.iter() {
-        // Skip virtual/non-existent cells (?) as well as non-cell boundaries
-        if cell.edge_marker != 0. {
-            continue;
-        }
-        let cbounds = &cell.cytoplasm_boundary;
-        for (x, y) in cbounds {
-            let (x, y) = (*x as f32, *y as f32);
-            let (x, y) = ((x - min_x).floor() as u32, (y - min_y).floor() as u32);
-            if x < abs_width && y < abs_height {
-                let mut pixel: &mut image::Rgba<u8> = img.get_pixel_mut(x as u32, y as u32);
-                let white = image::Rgba([255, 255, 255, 255]);
-                pixel.blend(&white);
-            }
-        }
-
-        let nbounds = &cell.nucleus_boundary;
-        for (x, y) in nbounds {
-            let (x, y) = (*x as f32, *y as f32);
-            let (x, y) = ((x - min_x).floor() as u32, (y - min_y).floor() as u32);
-            if x < abs_width && y < abs_height {
-                let mut pixel: &mut image::Rgba<u8> = img.get_pixel_mut(x as u32, y as u32);
-                let white = image::Rgba([255, 255, 255, 255]);
-                pixel.blend(&white);
-            }
-        }
+    if let Some(ref bounds) = cytoplasm_boundaries {
+        draw_points(&mut img, &transparent_white_rgba, &bounds);
     }
+    if let Some(ref bounds) = nucleus_boundaries {
+        draw_points(&mut img, &transparent_white_rgba, &bounds);
+    }
+
     if abs_height > abs_width {
         // We actually rotate the images 90° CW, so that width > height for better display properties.
         img = image::imageops::rotate90(&img)
