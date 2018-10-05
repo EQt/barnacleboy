29e55f235a261152abf2d7a166279c70878f2fe1
Till Hartmann <till.hartmann@udo.edu>
2018-10-05 10:58:22 +0200

    incorporate cellboundaries

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,22 +1,38 @@
-extern crate merfishtools;
-extern crate itertools;
 extern crate image;
+extern crate itertools;
+extern crate merfishtools;
 extern crate palette;
-extern crate cogset;
-extern crate rand;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_pickle;
 
-use rand::{thread_rng, Rng};
-
-
-use palette::{Srgba, Hsla};
-
-use merfishtools::io::merfishdata::Reader;
-use merfishtools::io::merfishdata::binary::Reader as MReader;
-use itertools::Itertools;
 use image::Pixel;
+use itertools::Itertools;
+use merfishtools::io::merfishdata::binary::Reader as MReader;
 use merfishtools::io::merfishdata::binary::Record;
+use merfishtools::io::merfishdata::Reader;
+use palette::{Hsla, Srgba};
 use std::collections::HashMap;
-use cogset::{Optics, BruteScan, Euclid, Dbscan, Kmeans};
+
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Cell {
+    #[serde(rename = "FOVNum")]
+    fovnum: f64,
+    #[serde(rename = "cellID")]
+    cell_id: f64,
+    cytoplasm_area: f64,
+    cytoplasm_boundary: Vec<(f64, f64)>,
+    cytoplasm_convex_hull_ratio: f64,
+    edge_marker: f64,
+    nucleus_area: f64,
+    nucleus_ave_intensity: f64,
+    nucleus_ave_intensity0: f64,
+    nucleus_boundary: Vec<(f64, f64)>,
+    nucleus_convex_hull_ratio: f64,
+}
 
 fn main() {
     // TODO use clap.g
@@ -36,64 +52,6 @@ fn main() {
     let abs_height = (max_y - min_y).abs().ceil() as u32;
     let mut img = image::ImageBuffer::from_pixel(abs_width, abs_height, image::Rgba([0u8, 0u8, 0u8, 255u8]));
     println!("{}x{}", abs_width, abs_height);
-
-    // Find cells by applying optics clustering
-    let mut reader = MReader::from_file(path).unwrap();
-    let points: Vec<Euclid<[f64; 2]>> = reader.records().into_iter().map(|r| r.unwrap())
-        .filter(|r| r.in_nucleus == 1)
-        .map(|r| r.abs_position)
-        .filter(|[x, y]| x - min_x < 500. && y - min_y < 500.)
-        .map(|[x, y]| Euclid([(x - min_x) as f64, (y - min_y) as f64]))
-        .collect();
-
-    println!("Num points: {}", points.len());
-
-    let scanner = BruteScan::new(&points);
-    let eps = 4.;
-    let min_pts = 30;
-
-//    let mut dbscan = Dbscan::new(scanner, eps, 2);
-//    let clusters = dbscan.by_ref().collect::<Vec<_>>();
-
-    let optics = Optics::new(scanner, eps, min_pts);
-    let mut clustering = optics.dbscan_clustering(eps);
-    let mut clusters = clustering.by_ref().collect::<Vec<_>>();
-    let noise = clustering.noise_points();
-
-//    let k = 75;
-//    let kmeans = Kmeans::new(&points, k);
-//    let mut clusters: Vec<Vec<usize>> = kmeans.clusters().into_iter().map(|(kmean, points)| points).collect();
-
-
-    let mut img = image::ImageBuffer::from_pixel(abs_width, abs_height, image::Rgba([0u8, 0u8, 0u8, 255u8]));
-    let num_clusters = clusters.len();
-    println!("Num clusters: {}", num_clusters);
-    thread_rng().shuffle(&mut clusters);
-    for (i, cluster) in clusters.into_iter().enumerate() {
-        let color = Hsla::new(((i as f32) / (num_clusters as f32)) * 360., 1.0, 0.5, 0.75);
-        let color = Srgba::from(color);
-        let data = [(color.color.red * 255.) as u8, (color.color.green * 255.) as u8, (color.color.blue * 255.) as u8, (color.alpha * 255.) as u8];
-        let rgba = image::Rgba(data);
-
-        for point_idx in cluster {
-            let Euclid(point) = points[point_idx];
-            let [x, y] = point;
-            let mut pixel: &mut image::Rgba<u8> = img.get_pixel_mut(x.floor() as u32, y.floor() as u32);
-            pixel.blend(&rgba);
-        }
-    }
-    for &point_idx in noise {
-        let Euclid(point) = points[point_idx];
-        let [x, y] = point;
-        let data = [255, 255, 255, 128];
-        let rgba = image::Rgba(data);
-        let mut pixel: &mut image::Rgba<u8> = img.get_pixel_mut(x.floor() as u32, y.floor() as u32);
-        pixel.blend(&rgba);
-    }
-
-    image::ImageRgba8(image::imageops::rotate90(&img)).save(format!("img/{}.png", "clusters")).unwrap();
-
-    std::process::exit(0);
 
     // create plot for every single barcode
     let mut reader = MReader::from_file(path).unwrap();
@@ -130,5 +88,39 @@ fn main() {
         }
         image::ImageLumaA8(barcode_img).save(format!("img/barcode_{}.png", key)).unwrap();
     }
-    image::ImageRgba8(image::imageops::rotate90(&img)).save(format!("img/{}.png", "absolute")).unwrap();
+
+    let f = std::fs::File::open("data/cellBoundaries.pickle").unwrap();
+    let cell_boundaries: HashMap<String, Cell> = serde_pickle::from_reader(f).unwrap();
+    for (_cell_id, cell) in cell_boundaries.iter() {
+        // Skip virtual/non-existent cells (?) as well as non-cell boundaries
+        if cell.edge_marker != 0. {
+            continue;
+        }
+        let cbounds = &cell.cytoplasm_boundary;
+        for (x, y) in cbounds {
+            let (x, y) = (*x as f32, *y as f32);
+            let (x, y) = ((x - min_x).floor() as u32, (y - min_y).floor() as u32);
+            if x < abs_width && y < abs_height {
+                let mut pixel: &mut image::Rgba<u8> = img.get_pixel_mut(x as u32, y as u32);
+                let white = image::Rgba([255, 255, 255, 255]);
+                pixel.blend(&white);
+            }
+        }
+
+        let nbounds = &cell.nucleus_boundary;
+        for (x, y) in nbounds {
+            let (x, y) = (*x as f32, *y as f32);
+            let (x, y) = ((x - min_x).floor() as u32, (y - min_y).floor() as u32);
+            if x < abs_width && y < abs_height {
+                let mut pixel: &mut image::Rgba<u8> = img.get_pixel_mut(x as u32, y as u32);
+                let white = image::Rgba([255, 255, 255, 255]);
+                pixel.blend(&white);
+            }
+        }
+    }
+    if abs_height > abs_width {
+        // We actually rotate the images 90° CW, so that width > height for better display properties.
+        img = image::imageops::rotate90(&img)
+    }
+    image::ImageRgba8(img).save(format!("img/{}.png", "absolute")).unwrap();
 }
