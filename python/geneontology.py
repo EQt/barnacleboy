0ae2c606f9030eb210eccaa59febc9780b730808
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-11-15 20:19:35 +0100

    make it a function

--- python/geneontology.py
+++ python/geneontology.py
@@ -3,28 +3,32 @@ Query GeneOntology from BioSmart service
 """
 from os import path
 import pandas as pd
-import biomart
 
 
 cb_path = "../data/codebook.csv"
 out_path = "../data/ontology.tsv"
-codes = pd.read_csv(cb_path, skiprows=3, skipinitialspace=True)
-gene_ids = codes['id'].dropna().tolist()
 
 
-server = biomart.BiomartServer('http://www.ensembl.org/biomart')
-data = server.datasets['hsapiens_gene_ensembl']
+def download_biomart():
+    import biomart
 
-res = data.search({
-    'filters': {'ensembl_transcript_id_version': gene_ids},
-    'attributes': [
-        'ensembl_transcript_id_version',
-        'ensembl_gene_id',
-        'name_1006',
-        'go_id',
-        'namespace_1003'
-    ]})
-assert res.ok
+    codes = pd.read_csv(cb_path, skiprows=3, skipinitialspace=True)
+    gene_ids = codes['id'].dropna().tolist()
 
-with open(out_path, 'w') as io:
-    io.write(res.text)
+
+    server = biomart.BiomartServer('http://www.ensembl.org/biomart')
+    data = server.datasets['hsapiens_gene_ensembl']
+
+    res = data.search({
+        'filters': {'ensembl_transcript_id_version': gene_ids},
+        'attributes': [
+            'ensembl_transcript_id_version',
+            'ensembl_gene_id',
+            'name_1006',
+            'go_id',
+            'namespace_1003'
+        ]})
+    assert res.ok
+
+    with open(out_path, 'w') as io:
+        io.write(res.text)
