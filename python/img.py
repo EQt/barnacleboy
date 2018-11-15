2894d871390f15a5a22a47bf1d91e57933d3610a
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-11-15 15:06:28 +0100

    plot transparent

--- python/img.py
+++ python/img.py
@@ -9,6 +9,12 @@ from reader import load_merfish
 from fisher import _test_file_name
 
 
+@njit(cache=True)
+def fill_img(img, x, y, v):
+    for i, (xi, yi) in enumerate(zip(x, y)):
+        img[xi, yi] += v[i]
+
+
 if __name__ == '__main__':
     import argparse
 
@@ -16,15 +22,12 @@ if __name__ == '__main__':
     p.add_argument('fname', nargs='*', default=[_test_file_name()])
     p.add_argument('-p', '--microns-per-pixel', type=float, default=2)
     p.add_argument('-t', '--threshold-percentile', type=float, default=0.01)
+    p.add_argument('-b', '--barcode', type=int, default=2)
     args = p.parse_args()
 
     mpp = args.microns_per_pixel
     cut = args.threshold_percentile
-
-    @njit(cache=True)
-    def fill_img(img, x, y, v):
-        for xi, yi, ai in zip(x, y, v):
-            img[xi, yi] += ai
+    cmap = plt.get_cmap('tab10')
 
     for fname in args.fname:
         out = path.basename(fname) + '.png'
@@ -43,9 +46,11 @@ if __name__ == '__main__':
         x = ((coords[:, 0] - min_x) / mpp).astype(int)
         y = ((coords[:, 1] - min_y) / mpp).astype(int)
         v = np.minimum(a['total_magnitude'] / vmax, 1.0)
-
+        color = np.array(cmap(0))
+        v4 = (np.array([v, v, v, v]).T * color)
         print(f'Filling image {width}x{height}')
-        img = np.zeros((width, height), dtype=float)
-        fill_img(img, x, y, v)
+        img = np.zeros((width, height, 4), dtype=float)
+        fill_img(img, x, y, v4)
+        img = np.minimum(img, 1.0)
         print(f'Saving to "{out}"')
         plt.imsave(out, img, cmap='gray', vmin=0, vmax=1.0)
