ff67902dd343bea217fd2a707b1df0e44fd520ec
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-11-16 15:18:40 +0100

    slow test

--- python/test_utils.py
+++ python/test_utils.py
@@ -16,3 +16,12 @@ def test_is_sorted1():
 def test_is_sorted2():
     a = np.array([0, 2, 2], dtype=np.uint8)
     assert is_sorted(a)
+
+
+def test_slow():
+    if False:
+        a = load_merfish(fname)
+        assert not is_sorted(a['cellID'])
+        cell_order = np.argsort(a['cellID'])
+        assert not is_sorted(cell_order)
+        assert is_sorted(a['cellID'][cell_order])
