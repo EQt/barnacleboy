f780b73619b16c5cfb3932da05e1743875ccfdb8
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-11-16 15:23:25 +0100

    what takes so long?

--- python/reduce.py
+++ python/reduce.py
@@ -1,25 +1,31 @@
 """
-Reduce the data to one node per cells
+Reduce the data to a graph having one node per cell
 """
 import matplotlib.pyplot as plt
 import numpy as np
+from os.path import basename
 from fisher import _test_file_name
 from reader import load_merfish
-from cells import is_sorted, group_index
+from utils import is_sorted, group_index
 from graph import delaunay_graph, plot_edges, euclidean_edge_length
 
 
 if __name__ == '__main__':
     fname = _test_file_name()
     df = load_merfish(fname)
+    print('Loading', basename(fname), end=' ... ', flush=True)
     a = np.array(df[['barcode_id', 'cellID', 'abs_position']])
-    a.sort(order='cellID')
+    print('[ok]')
+
+    print('Sorting cells', end=' ... ', flush=True)
+    cell_order = np.argsort(a['cellID'])
+    a = a[cell_order]
+    cell_idx = group_index(a['cellID'])
+    assert len(cell_idx) < 1e5
+    print('[ok]')
 
     if False:
         assert is_sorted(df['cellID'])
-    cell_idx = group_index(a['cellID'])
-    assert len(cell_idx) < 1e5
-
 
     centers = np.array([
         p.mean(axis=0) for p in np.split(a['abs_position'], cell_idx[1:])
