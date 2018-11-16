2c9e8b39291874cb37ef00b1d68e1ebc5ba4bae3
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-11-16 15:05:08 +0100

    found a bug in is_sorted

--- python/test_utils.py
+++ python/test_utils.py
@@ -1,7 +1,13 @@
 import numpy as np
+from utils import is_sorted
 
 
 def test_reshape():
     """Check that my assumptions on how reshape on a 3d array work"""
     a = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]], [[9, 10], [11, 12]]])
     assert a.shape == (3, 2, 2)
+
+
+def test_is_sorted1():
+    a = np.array([1, 2, 0], dtype=np.uint8)
+    assert not is_sorted(a)
