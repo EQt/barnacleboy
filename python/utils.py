028f41020d1d6a0e06543be096d1ca53efe69515
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-11-22 12:48:33 +0100

    class Status

--- python/utils.py
+++ python/utils.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 from typing import List
 
@@ -19,3 +20,25 @@ def group_index(arr) -> List:
 def unique_elements(arr) -> List:
     """Assumes that arr is sorted"""
     return arr[group_index(arr)]
+
+
+class Status:
+    """
+    Show status message when beginning and [ok] when done on `sys.stdout`.
+
+    Example:
+    >>> with Status("call slow"):
+    >>>     slow(bli, bla)
+    """
+    indent = 40
+
+    def __init__(self, msg, out=sys.stdout):
+        self.msg = msg
+        self.out = out
+
+    def __enter__(self):
+        print(self.msg.ljust(type(self).indent), end='...', flush=True,
+              file=self.out)
+
+    def __exit__(self, *args):
+        print(' [ok]', file=self.out)
