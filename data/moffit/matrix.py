a9edb677519a203a517947e84226b86c28e3425a
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-11-27 17:39:50 +0100

    found out what the matrix contains: gene to barcode mapping

--- data/moffit/matrix.py
+++ data/moffit/matrix.py
@@ -2,8 +2,10 @@
 Load the matrix provided on GEO (gene expression omnibus)
 """
 import scipy.io
+import pandas as pd
 
-fname = "./raw/GSE113576_matrix.mtx.gz"
+gse = "GSE113576"
+fname = f"./raw/{gse}_matrix.mtx.gz"
 mat = scipy.io.mmread(fname)
 
 n, m = mat.shape
@@ -11,3 +13,9 @@ assert mat.dtype.name == 'int64'
 assert n == 27998
 assert m == 31299
 assert round(mat.nnz / (n*m), 3) == 0.096
+
+genes = pd.read_csv(f"./raw/{gse}_genes.tsv.gz", sep='\t', header=None)
+assert len(genes) == n
+
+barcodes = pd.read_csv(f"./raw/{gse}_barcodes.tsv.gz", sep='\t', header=None)
+assert len(barcodes) == m
