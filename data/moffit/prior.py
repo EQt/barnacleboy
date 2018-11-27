8acab6bc23ea7ac225a5c96503c90dace3c44535
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-11-27 14:54:12 +0100

    quick look onto pixel.csv

---
+++ data/moffit/prior.py
@@ -0,0 +1,28 @@
+"""
+Let's have a quick look onto the data
+"""
+import pandas as pd
+
+fname = 'raw/pixel.csv'
+
+# show all columns
+df = pd.read_csv(fname, nrows=10)
+for c in df.columns:
+    print(c)
+
+
+# how many cells?
+if False:
+    df = pd.read_csv(fname, usecols=['Cell_ID'])
+    print(f"#lines = {len(df):,d}")
+    print(f"There are {len(df['Cell_ID'].unique()):,d} cells")
+    # result: all cells are unique
+
+
+# animal ...
+df = pd.read_csv(fname, usecols=['Animal_sex', 'Animal_ID'])
+print(f"{len(df.Animal_ID.unique())} different animals involved")
+print(f"{(df.Animal_sex == 'Male').sum() / len(df) * 100:.2f}% are male")
+
+# https://en.wikipedia.org/wiki/Bregma
+#   Coordinate system in the skull
