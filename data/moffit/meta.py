14cd4bfd6777fd045fe998e954c8027b42142272
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-11-27 14:44:29 +0100

    show metadata

---
+++ data/moffit/meta.py
@@ -0,0 +1,25 @@
+"""
+Extract description of the CSV columns of 
+"""
+from lxml import etree
+from io import BytesIO
+
+
+fname = 'raw/pixel.meta.xml'
+xml = etree.parse(fname)
+ns = xml.getroot().nsmap[None]
+
+head, = xml.xpath('//i:metadata[@element="xhtml_head_item"]', namespaces={'i': ns})
+
+content = etree.tostring(head)
+content = content.replace(b"&gt;", b">")
+content = content.replace(b"&lt;", b"<")
+
+io = BytesIO(content)
+x = etree.parse(io)
+assert ns == x.getroot().nsmap[None]    # same namespace
+
+[descr] = x.xpath('//i:meta[@name="DC.description"]/@content', namespaces={'i': ns})
+
+for s in descr.split('.'):
+    print(s)
