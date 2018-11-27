e31b47eef70c06bf35e1bc3a29620e280fbff6b0
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-11-27 18:48:54 +0100

    speedup matrix loading

--- data/moffit/matrix.py
+++ data/moffit/matrix.py
@@ -13,13 +13,20 @@ import pandas as pd
 
 gse = "GSE113576"
 fname = f"./raw/{gse}_matrix.mtx.gz"
-mat = scipy.io.mmread(fname)
 
-n, m = mat.shape
-assert mat.dtype.name == 'int64'
+
+if False:
+    mat = scipy.io.mmread(fname)
+    n, m = mat.shape
+    nnz = mat.nnz
+    assert mat.dtype.name == 'int64'
+else:
+    n, m, nnz = pd.read_csv(fname, sep=' ', skiprows=2, nrows=1, header=None).values[0]
+    # vals = pd.read_csv(fname, sep=' ', skiprows=3, header=None)
+
 assert n == 27998
 assert m == 31299
-assert round(mat.nnz / (n*m), 3) == 0.096
+assert round(nnz / (n*m), 3) == 0.096
 
 genes = pd.read_csv(f"./raw/{gse}_genes.tsv.gz", sep='\t', header=None)
 assert len(genes) == n
