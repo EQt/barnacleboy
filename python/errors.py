c44bbdc3bc703303ff6eb407a806f954150583a1
Till Hartmann <till.hartmann@udo.edu>
2018-12-05 10:15:06 +0100

    have a look at blanks

--- python/errors.py
+++ python/errors.py
@@ -1,29 +1,31 @@
 import click
 import numpy as np
+import pandas as pd
 from reader import load_merfish
 
 
 @click.command()
-@click.argument("FILE", type=click.Path())
-def error(file: str):
+@click.argument("MERFISH", type=click.Path())
+@click.argument("CODEBOOK", type=click.Path())
+def estimate_errors(merfish: str, codebook: str):
     """Calculate field-of-view dimensions and overlap.
     """
-    data = load_merfish(file)[['barcode', 'error_bit']]
+    data = load_merfish(merfish)[['barcode', 'error_bit']]
     num_records = data.shape[0]
     error_bits = np.array(data['error_bit'], dtype=np.uint32)
     barcodes = data['barcode']
 
     correction = 1 << (error_bits - 1)  # error_bit == 0 → no error, error_bit == n → error at position (n-1)
     correction[error_bits == 0] = 0
-    corrected_barcodes = barcodes ^ correction
+    uncorrected_barcodes = barcodes ^ correction
     num_corrected_barcodes = np.count_nonzero(error_bits)
-    assert num_corrected_barcodes == np.sum(corrected_barcodes != barcodes)
+    assert num_corrected_barcodes == np.sum(uncorrected_barcodes != barcodes)
 
     one_to_zero_errors = (barcodes & correction)
     zero_to_one_errors = ((~barcodes) & correction)
 
     print("One → Zero errors (for each index):")
-    sel = (1 << np.arange(0, 16)).reshape((16, -1))
+    sel = (1 << np.arange(0, 16)).reshape((-1, 1))
     print('\n'.join(map(str, zip(range(16), np.sum(one_to_zero_errors == sel, axis=1) / num_records))))
     # for i in range(16):
     #     print(f"{i}: {np.sum(one_to_zero_errors == (1 << i)) / num_records}")
@@ -34,6 +36,20 @@ def error(file: str):
     # for i in range(16):
     #     print(f"{i}: {np.sum(zero_to_one_errors == (1 << i)) / num_records}")
 
+    codes = pd.read_csv(codebook, skiprows=3, skipinitialspace=True,
+                        usecols=['name', 'barcode'],
+                        dtype={'name': str, 'barcode': str})
+    blanks = codes[codes['name'].str.startswith('Blank-')]
+    blank_barcodes_str = blanks['barcode'].values
+    blank_barcodes = np.array(list(map(lambda v: int(v, base=2), blank_barcodes_str))).reshape((-1, 1))
+
+    print()
+    print(f"Assigned blank counts (pre-correction):\n"
+          f"{dict(zip(blank_barcodes_str, np.sum(uncorrected_barcodes == blank_barcodes, axis=1)))}")
+    print()
+    print(f"Assigned blank counts (post-correction):\n"
+          f"{dict(zip(blank_barcodes_str, np.sum(barcodes == blank_barcodes, axis=1)))}")
+
 
 if __name__ == "__main__":
-    error()
+    estimate_errors()
